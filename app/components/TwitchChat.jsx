755b7ecd7dbdc5df5682acde227a11e4b61393f9
Manoel Da Ponte <131278442+ManoelDaPonte@users.noreply.github.com>
2025-03-12 16:21:27 +0100

    dev

--- app/components/TwitchChat.jsx
+++ app/components/TwitchChat.jsx
@@ -4,52 +4,52 @@ import { useTextToSpeech } from "@/api/QueryTextToSpeech";
 import styles from "@/styles/twitchChat.module.css";
 
 function TwitchChat({ channelName, messagesState, lastMessageState }) {
-    const [messages, setMessages] = messagesState;
-    const [lastMessage, setLastMessage] = lastMessageState;
+	const [messages, setMessages] = messagesState;
+	const [lastMessage, setLastMessage] = lastMessageState;
 
-    const { playAudioFromText, isLoading, error } = useTextToSpeech();
+	const { playAudioFromText, isLoading, error } = useTextToSpeech();
 
-    useEffect(() => {
-        const client = new Client({
-            channels: [channelName],
-        });
+	useEffect(() => {
+		const client = new Client({
+			channels: [channelName],
+		});
 
-        client.connect();
+		client.connect();
 
-        client.on("message", (channel, tags, message, self) => {
-            if (!self) {
-                const newMessage = {
-                    displayName: tags["display-name"],
-                    message,
-                };
-                setMessages((prevMessages) => [...prevMessages, newMessage]);
-                setLastMessage(newMessage);
-            }
-        });
+		client.on("message", (channel, tags, message, self) => {
+			if (!self) {
+				const newMessage = {
+					displayName: tags["display-name"],
+					message,
+				};
+				setMessages((prevMessages) => [...prevMessages, newMessage]);
+				setLastMessage(newMessage);
+			}
+		});
 
-        return () => {
-            client.disconnect();
-        };
-    }, [channelName]);
+		return () => {
+			client.disconnect();
+		};
+	}, [channelName]);
 
-    return (
-        <div className={styles.chatContainer}>
-            <div className={styles.header}>{channelName}.tv</div>
-            <div className={styles.chat}>
-                {messages.map((msg, index) => (
-                    <div
-                        key={index}
-                        className={styles.chatMessage}
-                        onClick={() => playAudioFromText(msg.message)}
-                    >
-                        <div className={styles.userName}>{msg.displayName}</div>
-                        <div className={styles.point}>:</div>
-                        <div className={styles.message}>{msg.message}</div>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+	return (
+		<div className={styles.chatContainer}>
+			<div className={styles.header}>{channelName}.tv</div>
+			<div className={styles.chat}>
+				{messages.map((msg, index) => (
+					<div
+						key={index}
+						className={styles.chatMessage}
+						onClick={() => playAudioFromText(msg.message)}
+					>
+						<div className={styles.userName}>{msg.displayName}</div>
+						<div className={styles.point}>:</div>
+						<div className={styles.message}>{msg.message}</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default TwitchChat;
