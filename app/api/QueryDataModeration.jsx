b5e86f7403c5985d3cfffcc04a018392c8da0df1
Manoel Da Ponte <131278442+ManoelDaPonte@users.noreply.github.com>
2025-03-14 12:11:03 +0100

    dev api vercel

--- app/api/QueryDataModeration.jsx
+++ app/api/QueryDataModeration.jsx
@@ -1,14 +1,17 @@
 export const fetchDataModeration = async (userInput) => {
-    if (!userInput?.message) {
-        // Early return or throw an error if userInput.message is not valid
-        throw new Error("userInput.message is required");
-    }
+	const API_BASE_URL =
+		process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
-    const response = await fetch(
-        `http://127.0.0.1:8000/moderation/${userInput.message}`
-    );
-    if (!response.ok) {
-        throw new Error("Network response was not ok");
-    }
-    return response.json();
+	if (!userInput?.message) {
+		// Early return or throw an error if userInput.message is not valid
+		throw new Error("userInput.message is required");
+	}
+
+	const response = await fetch(
+		`${API_BASE_URL}/moderation/${userInput.message}`
+	);
+	if (!response.ok) {
+		throw new Error("Network response was not ok");
+	}
+	return response.json();
 };
--- app/api/QueryModelisation.jsx
+++ app/api/QueryModelisation.jsx
@@ -1,31 +1,33 @@
 import { useState, useCallback } from "react";
 
 export function useQueryTopicsModelisation(endpoint) {
-    const [data, setData] = useState(null);
+	const [data, setData] = useState(null);
 
-    // This function is what we'll call to trigger the data fetch
-    const fetchData = useCallback(async (messages) => {
-        const comments = messages.map((messageObject) => messageObject.message);
-        console.log(comments); // Debug: Log the comments being sent
+	// This function is what we'll call to trigger the data fetch
+	const fetchData = useCallback(async (messages) => {
+		const comments = messages.map((messageObject) => messageObject.message);
+		const API_BASE_URL =
+			process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+		console.log(comments); // Debug: Log the comments being sent
 
-        try {
-            const response = await fetch(`http://localhost:8000/${endpoint}/`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ comments }),
-            });
+		try {
+			const response = await fetch(`${API_BASE_URL}/${endpoint}/`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ comments }),
+			});
 
-            if (response.ok) {
-                const json = await response.json();
-                setData(json);
-            } else {
-                console.error("Error:", response.statusText);
-            }
-        } catch (error) {
-            console.error("Error:", error);
-        }
-    }, []);
-    return [data, fetchData];
+			if (response.ok) {
+				const json = await response.json();
+				setData(json);
+			} else {
+				console.error("Error:", response.statusText);
+			}
+		} catch (error) {
+			console.error("Error:", error);
+		}
+	}, []);
+	return [data, fetchData];
 }
